24678ea846560dcd68b23d565bd5f8b2e607d051
bosung si <qweasdzxc0311@naver.com>
2024-05-04 23:51:08 +0900

    채널 개별 수정 및 조회 유효성 검사 추가

--- Youtuber/routes/channel.js
+++ Youtuber/routes/channel.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const conn = require('../db');
-const { body, validationResult } = require('express-validator')
+const { body, param, validationResult } = require('express-validator')
 
 
 router.use(express.json());
@@ -9,27 +9,39 @@ router.use(express.json());
 // 채널 전체 조회, 채널 생성
 router
     .route('/')
-    .get((req, res) => {
-        let { userId } = req.body;
-        
-        let sql = `SELECT * FROM channel WHERE user_id = ?`
-        userId ? conn.query(sql, userId,
-            function (err, results) {
-                if (results.length) {
-                    res.status(200).json(results);    
-                } else {
-                    res.status(404).json({
-                        message : "채널 정보를 찾을 수 없습니다."
-                    })
-                }
+    .get(body('userId').notEmpty().isInt().withMessage('userId는 숫자로 입력하셔야합니다.')
+        , (req, res) => {
+            const err = validationResult(req);
+
+            if (!err.isEmpty()) {
+                return res.status(400).json(err.array());
             }
-        )
-        :
-        res.status(400).end();
-    })
+
+            let { userId } = req.body;
+            
+            let sql = `SELECT * FROM channel WHERE user_id = ?`
+            conn.query(sql, userId,
+                function (err, results) {
+                    if (err) {
+                        console.log(err);
+                        return res.status(400).end();
+                    }
+
+                    if (results.length) {
+                        res.status(200).json(results);    
+                    } else {
+                        res.status(404).json({
+                            message : "채널 정보를 찾을 수 없습니다."
+                        })
+                    }
+                }
+            )
+            
+
+        })
     .post(
         [
-            body('userId').notEmpty().isInt().withMessage('userId는 숫자만 가능합니다.'),
+            body('userId').notEmpty().isInt().withMessage('userId는 숫자로 입력하셔야합니다.'),
             body('name').notEmpty().isString().withMessage('name은 문자로 입력하셔야합니다.')
         ]
         , (req, res) => {
@@ -58,43 +70,68 @@ router
 // 채널 개별 조회, 채널 개별 수정, 채널 개별 삭제
 router
     .route('/:id')
-    .get((req, res) => {
-        let { id } = req.params;
-        id = parseInt(id);
-        
-        let sql = `SELECT * FROM channel WHERE id = ?`
-        conn.query(sql, id,
-            function (err, results) {
-                if (results.length) {
-                    res.status(200).json(results);    
-                } else {
-                    res.status(404).json({
-                        message : "채널 정보를 찾을 수 없습니다."
-                    })
-                }
+    .get(param('id').notEmpty().withMessage('채널 id 필요')
+        , (req, res) => {
+
+            const err = validationResult(req);
+
+            if (!err.isEmpty()) {
+                return res.status(400).json(err.array());
             }
-        )
-    })
-    .put((req, res) => {
-        let { id } = req.params;
-        id = parseInt(id);
 
-        let channel = db.get(id);
-        let oldTitle = channel.channelTitle;
+            let { id } = req.params;
+            id = parseInt(id);
+            
+            let sql = `SELECT * FROM channel WHERE id = ?`
+            conn.query(sql, id,
+                function (err, results) {
+                    if (err) {
+                        console.log(err);
+                        return res.status(400).end();
+                    }
 
-        if (channel) {
-            let newTitle = req.body.channelTitle;
-            channel.channelTitle = newTitle;
-            db.set(id, channel);
-            res.json({
-                message : `채널명이 정상적으로 수정되었습니다. 기존 ${oldTitle}에서 수정 ${newTitle}`
-            })
-        } else {
-            res.status(404).json({
-                message: "채널 정보를 찾을 수 없습니다."
-            })
-        }
-    })
+                    if (results.length) {
+                        res.status(200).json(results);    
+                    } else {
+                        res.status(404).json({
+                            message : "채널 정보를 찾을 수 없습니다."
+                        })
+                    }
+                }
+            )
+        })
+    .put([
+        param('id').notEmpty().withMessage('채널 id 필요'),
+        body('name').notEmpty().isString().withMessage('채널명 오류')
+        ]
+        , (req, res) => {
+            const err = validationResult(req);
+
+            if (!err.isEmpty()) {
+                return res.status(400).json(err.array());
+            }
+
+            let { id } = req.params;
+            id = parseInt(id);
+            let { name } = req.body;
+
+            let sql = `UPDATE channel SET name = ? WHERE id = ?`
+            let values = [name, id];
+            conn.query(sql, values,
+                function (err, results) {
+                    if (err) {
+                        console.log(err);
+                        return res.status(400).end();
+                    }
+
+                    if (results.affectedRows == 0) {
+                        return res.status(400).end();
+                    }
+                    
+                    res.status(200).json(results);    
+                }
+            )
+        })
     .delete((req, res) => {
         let { id } = req.params;
         id = parseInt(id);
