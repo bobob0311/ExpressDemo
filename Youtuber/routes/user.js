0973a4581c7126fd4271af87c35b3550ff5beac0
bosung si <qweasdzxc0311@naver.com>
2024-05-06 19:26:28 +0900

    유효성 검사 수정

--- Youtuber/routes/user.js
+++ Youtuber/routes/user.js
@@ -1,81 +1,127 @@
 const express = require('express');
 const router = express.Router();
 const conn = require('../db');
+const { body, param,  validationResult } = require('express-validator')
 
 router.use(express.json());
 
+const validate = (req, res, next) => {
+    const err = validationResult(req);
 
+    if (err.isEmpty()) {
+        return next();
+    } else {
+        return res.status(400).json(err.array());
+    }
+}
 
 // 로그인
-router.post('/login', (req, res) => {
-    const { email, password } = req.body;
-    const sql = 'SELECT * FROM `users` WHERE email = ?';
+router.post(
+    '/login',
+    [
+        body('email').notEmpty().isEmail().withMessage("이메일 확인이 필요합니다"),
+        body('password').notEmpty().isString().withMessage("비밀번호 확인 필요"),
+        validate
+    ],
+    (req, res) => {
+        const { email, password } = req.body;
+        const sql = 'SELECT * FROM `users` WHERE email = ?';
 
-    conn.query(sql, email,
-        function (err, results) {
-            const loginUser = results[0];
-            
-            if (loginUser && loginUser.password == password) {
-                res.status(200).json({
-                    message: `${loginUser.name}님 로그인 되었습니다.`
-                })
-            } else {
-                res.status(404).json({
-                    message : "이메일 또는 비밀번호가 틀렸습니다."
-                })
+        conn.query(sql, email,
+            function (err, results) {
+                if (err) {
+                        console.log(err);
+                        return res.status(400).end();
+                    }
+                    
+                const loginUser = results[0];
+                
+                if (loginUser && loginUser.password == password) {
+                    res.status(200).json({
+                        message: `${loginUser.name}님 로그인 되었습니다.`
+                    })
+                } else {
+                    res.status(404).json({
+                        message : "이메일 또는 비밀번호가 틀렸습니다."
+                    })
+                }
             }
-        }
-    );    
-})
+        );    
+    })
 
 // 회원 가입
-router.post('/join', (req, res) => {
-    if (req.body == {}) {
-        res.status(400).json({
-            message : '입력 값을 다시 확인해주세요'
-        })
-    } else {
+router.post(
+    '/join',
+    [   
+        body('email').notEmpty().isEmail().withMessage("이메일 확인이 필요합니다"),
+        body('name').notEmpty().isString().withMessage("이름 확인 필요"),
+        body('password').notEmpty().isString().withMessage("비밀번호 확인 필요"),
+        body('contact').notEmpty().isString().withMessage("연락처 확인 필요"),
+        validate
+    ],
+    (req, res) => {
         const { email, name, password, contact } = req.body;
 
         const sql = 'INSERT INTO users (email, name, password, contact) VALUES (?, ?, ?, ?)';
         const values = [email, name, password, contact];
         conn.query(sql, values,
             function (err, results) {
+                if (err) {
+                    console.log(err);
+                    return res.status(400).end();
+                }
+
                 res.status(201).json(results);    
             }
-        );
-    }
-               
-})
+        );  
+    })
 
 // 회원 조회 , 회원 삭제 
 router
     .route('/users')
-    .get( (req,res) => {
-        const { email } = req.body;
-    
-        const sql = 'SELECT * FROM `users` WHERE email = ?';
-        conn.query(sql,email,
-            function (err, results, fields) {
-                if (results.length) {
-                    res.status(200).json(results);    
-                } else {
-                    res.status(404).json({
-                        message : "회원 정보가 없습니다."
-                    })
-                }
-            }
-        );
-    })
-    .delete((req,res) => {
-        const { email } = req.body;
+    .get(
+        [
+            body('email').notEmpty().isEmail().withMessage("이메일 확인이 필요합니다"),
+            validate
+        ] 
+        , (req, res) => {
+            const { email } = req.body;
         
-        const sql = 'DELETE FROM users WHERE email = ?';
-        conn.query(sql, email,
-            function (err, results, fields) {
-                res.status(200).json(results);
-            }
-        );
+            const sql = 'SELECT * FROM `users` WHERE email = ?';
+            conn.query(sql,email,
+                function (err, results) {
+                    if (err) {
+                        console.log(err);
+                        return res.status(400).end();
+                    }
+
+                    res.status(200).json(results);
+                }
+            );
+    })
+    .delete(
+        [
+            body('email').notEmpty().isEmail().withMessage("이메일 확인이 필요합니다"),
+            validate            
+        ]
+        , (req, res) => {
+            const { email } = req.body;
+            
+            const sql = 'DELETE FROM users WHERE email = ?';
+            conn.query(sql, email,
+                function (err, results) {
+                    if (err) {
+                        console.log(err);
+                        return res.status(400).end();
+                    }
+
+                    if (results.affectedRows == 0) {
+                        return res.status(400).end();
+                    }
+
+                    res.status(200).json(results);    
+                }
+            );
     })
 
 
